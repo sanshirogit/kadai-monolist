28e60efabc3376066a94e9da7702ca65d8b90676
Hironori Tanimura <over88keys@yahoo.co.jp>
2017-11-21 13:27:22 +0000

    withTimestamps rewrite

--- app/Item.php
+++ app/Item.php
@@ -10,7 +10,7 @@ class Item extends Model
     
     public function users()
     {
-        return $this->belongsToMany(User::class)->withPivot('type');
+        return $this->belongsToMany(User::class)->withPivot('type')->withTimestamps();
     }
     
     public function want_users()
