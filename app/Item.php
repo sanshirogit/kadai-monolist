70072ae5e475900bc33c29cc92d7fc11d13ea4f0
Hironori Tanimura <over88keys@yahoo.co.jp>
2017-11-21 12:25:24 +0000

    withTimestamps delete

--- app/Item.php
+++ app/Item.php
@@ -10,7 +10,7 @@ class Item extends Model
     
     public function users()
     {
-        return $this->belongsToMany(User::class)->withPivot('type')->withTimestamps();
+        return $this->belongsToMany(User::class)->withPivot('type');
     }
     
     public function want_users()
